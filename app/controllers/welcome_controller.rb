dad23838f8089e4e49f5464a346954c5636fd936
Sergey <mastedm@gmail.com>
2015-02-04 11:42:37 +0300

    order articles on index page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
   before_filter :set_title
   
   def index
-    @articles = Article.last(10)    
+    @articles = Article.order('created_at desc').limit(10)   
   end
   
   def category
